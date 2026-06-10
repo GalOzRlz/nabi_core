e52b5a2e93fe3058598e444904227d6b21d675b9
GalOzRlz <gal.y.oz@proton.me>
2026-06-10 20:35:21 +0300

    lower voices and buffer?

--- src/ios/synth.rs
+++ src/ios/synth.rs
@@ -127,7 +127,7 @@ impl<const N: usize> Synth<N> for SynthPlayer<N> {
         let buffer_size = match buffer_size_range {
             // If the device reports a min/max range, pick a value in between
             SupportedBufferSize::Range { min, max } => {
-                let target = 442; // todo: make it configurable?
+                let target = 390; // todo: make it configurable?
                 // Clamp the target to the valid range [min, max]
                 let chosen = target.clamp(*min, *max);
                 println!(
