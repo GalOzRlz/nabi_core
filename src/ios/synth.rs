dcc984189576d6b92debc09ac875afcb6c3309ef
GalOzRlz <gal.y.oz@proton.me>
2026-06-10 20:46:18 +0300

    shorter crossfade?

--- src/ios/synth.rs
+++ src/ios/synth.rs
@@ -401,7 +401,7 @@ impl<const N: usize> VoiceManager<N> {
         let entry = &self.patch_table.entries[self.current_patch_num].effects;
         let new_fx_net = entry.clone().build_chain(&self.states[0]);
         self.mix_net
-            .crossfade(self.fx_node_id, Fade::Smooth, 0.5, Box::new(new_fx_net));
+            .crossfade(self.fx_node_id, Fade::Smooth, 0.1, Box::new(new_fx_net));
     }
 
     /// Commit patch Net changes (sound rebuilt, effects chain rebuild, etc.)
