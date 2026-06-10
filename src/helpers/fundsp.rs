0456f70b2c7e18843a580e77ea35861009a7f56f
GalOzRlz <gal.y.oz@proton.me>
2026-06-10 20:11:03 +0300

    drop voices

--- src/helpers/fundsp.rs
+++ src/helpers/fundsp.rs
@@ -2,6 +2,6 @@ use fundsp::audionode::AudioNode;
 use fundsp::combinator::An;
 use fundsp::net::Net;
 
-pub fn to_net<F: AudioNode + 'static>(fx: An<F>) -> Net {
-    Net::wrap(Box::new(fx))
+pub fn to_net<F: AudioNode + 'static>(node: An<F>) -> Net {
+    Net::wrap(Box::new(node))
 }
