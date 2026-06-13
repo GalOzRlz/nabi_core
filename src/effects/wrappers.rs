c589bd4754a6c48744fcac1d94321ea59cfbff2c
GalOzRlz <gal.y.oz@proton.me>
2026-06-13 19:16:02 +0300

    add some documentation  still no testing

--- src/effects/wrappers.rs
+++ src/effects/wrappers.rs
@@ -5,19 +5,35 @@ use fundsp::typenum::{Const, ToUInt, U, U2};
 use fundsp::{Frame, Size};
 use std::sync::Arc;
 
-/// Generic wrapper for stereo effects (input 0,1 is audio) which have only f32 params (e.g., reverb_stereo) in their signature.
-/// A convenience function that assembles the effect from an array of N static params is wrapped in this struct.
-/// This allows for cc values to change the effect on the fly - with the effect being rebuilt on each cc change.
+type GenericStereoToN<const N: usize> = Arc<dyn Fn([f32; N]) -> Net + Send + Sync>;
+
+/// Generic wrapper for stereo effects (where input 0 and 1 are mapped for audio) which have only f32 params in their signature.
+/// A convenience closure that assembles the effect from an array of N ( 2 audio inputs + function params controlled by cc)
+/// which can then be changed via Net::pipe.
+/// This allows for modulation (e.g., cc Shared type) to change the effect on the fly - with the effect being rebuilt only when needed.
+/// By convention 0,1 inputs are audio and the rest will be the params, in the order that the closure expects them to appear.
+///
+/// ### Example
+/// ```
+/// let full_cc_reverb = StereoFXStaticParamsWrapper::<6>::new(Arc::new(
+/// move |args: [f32; 6]| {
+/// /// args[0], args[1] are audio (ignored here, but still passed through)
+/// reverb_stereo(args[2], args[3], args[4], args[5])
+/// }
+/// ));
+/// /// all inputs are now piped into the wrapper!
+/// ( pass() | pass() | cc_1 | cc_2 |cc_3 | cc_4) >> full_cc_reverb
+/// ```
 #[derive(Clone)]
 struct StereoFXStaticParamsWrapper<const N: usize> {
-    inner: Arc<dyn Fn([f32; N]) -> Net + Send + Sync>,
+    inner: GenericStereoToN<N>,
     effect: Net,
     params_state: [f32; N],
     params_temp: [f32; N],
 }
 
 impl<const N: usize> StereoFXStaticParamsWrapper<N> {
-    fn new(inner: Arc<dyn Fn([f32; N]) -> Net + Send + Sync>) -> Self {
+    fn new(inner: GenericStereoToN<N>) -> Self {
         StereoFXStaticParamsWrapper {
             inner,
             params_temp: [0.0; N],
