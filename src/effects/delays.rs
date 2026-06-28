6572d7b5acd519a141eaf76831d5b3168b907476
GalOzRlz <gal.y.oz@proton.me>
2026-06-28 16:39:36 +0300

    add simple delay, still no filter for wet yet

--- src/effects/delays.rs
+++ src/effects/delays.rs
@@ -2,15 +2,16 @@ use crate::common::params::CcAudioNode;
 use fundsp::audionode::FrameId;
 use fundsp::combinator::An;
 use fundsp::feedback::Feedback;
-use fundsp::prelude64::{Net, U1, Unit, feedback, pass, tap, unit};
+use fundsp::prelude64::{Net, U1, Unit, feedback, tap, unit};
 
-fn tap_delay(
+fn cc_controlled_delay(
     min_delay: f32,
     max_delay: f32,
     feedback_cc: Net,
+    audio_net: Net,
     delay_time: CcAudioNode,
 ) -> An<Feedback<U1, Unit<U1, U1>, FrameId<U1>>> {
-    let delay_line = (pass() | delay_time) >> tap(min_delay, max_delay) * feedback_cc;
+    let delay_line = (audio_net | delay_time) >> tap(min_delay, max_delay) * feedback_cc;
     let db = unit::<U1, U1>(Box::new(delay_line));
     feedback(db)
 }
