8d692543455669bd99780f97b31623ff8700ef28
GalOzRlz <gal.y.oz@proton.me>
2026-06-30 15:34:00 +0300

    added most of the data into the synth function considering non cc params for filter depth - for now plan is 5_000 max range as default

--- src/sound_engine/proph6.rs
+++ src/sound_engine/proph6.rs
@@ -21,13 +21,46 @@ struct Proph6 {
 }
 
 pub fn proph6(state: &SharedMidiState, params: &Parameterized) -> Box<dyn AudioUnit> {
+    // osc A
+    let osc_a1 = params.get_non_cc_param("osc_a1").unwrap();
+    let osc_a2 = params.get_non_cc_param("osc_a2").unwrap();
+    let osc_a3 = params.get_non_cc_param("osc_a2").unwrap();
+    let osc_a_level = params.sound_cc_or_default("osc_a_level", state);
+    let osc_a_pitch_shift = params.sound_cc_or_default("osc_a_pitch_shift", state);
+
+    // osc B
+    let osc_b1 = params.get_non_cc_param("osc_b1").unwrap();
+    let osc_b2 = params.get_non_cc_param("osc_b2").unwrap();
+    let osc_b3 = params.get_non_cc_param("osc_b2").unwrap();
+    let osc_b_level = params.sound_cc_or_default("osc_b_level", state);
+
+    // filter
+    // cut off, q ..
+    let filter_evn_amount = (params
+        .sound_cc_or_map("lfo_freq", state, |x| x.value.as_zero_to_one_f32().unwrap())
+        * 2.0)
+        - 1.0; // -1 to +1
+
+    // envelops
+    let (a, d, s, r) = params.get_cc_adsr_params("attack", "decay", "sustain", "release", state);
+    let (mod_a, mod_d, mod_s, mod_r) = params.get_cc_adsr_params(
+        "mod_attack",
+        "mod_decay",
+        "mod_sustain",
+        "mod_release",
+        state,
+    );
+
+    // modulation
+    let osc_b_mod_depth = params.sound_cc_or_default("osc_b_mod", state);
     let lfo_freq = params.sound_cc_or_default("lfo_freq", state);
     let lfo_depth = params.sound_cc_or_default("lfo_freq", state);
     let lfo_string = params
         .get_non_cc_param("lfo_shape")
-        .unwrap()
+        .expect("did not provide proper lfo shape!")
         .value
         .to_string();
     let lfo_node = lfo_freq * lfo_depth >> LFO::from_str(lfo_string.as_str()).unwrap().get_node();
+
     todo!("still incomplete")
 }
